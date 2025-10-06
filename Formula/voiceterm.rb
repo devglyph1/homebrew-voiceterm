e07fb419771ef8444c2a032dac42fe291497421a
satishChadive <satish.chandra@cashfree.com>
2025-10-06 19:02:31 +0530

    improved UI

--- Formula/voiceterm.rb
+++ Formula/voiceterm.rb
@@ -1,9 +1,9 @@
 class Voiceterm < Formula
   desc "A voice-powered AI terminal assistant"
   homepage "https://github.com/devglyph1/homebrew-voiceterm"
-  url "https://github.com/devglyph1/homebrew-voiceterm/releases/download/v0.1.2/voiceterm.tar.gz"
-  sha256 "6174b82d3e8e382a21f818e60601f997aab42ff247e4409513dd309006b367a9"
-  version "0.1.2"
+  url "https://github.com/devglyph1/homebrew-voiceterm/releases/download/v0.1.3/voiceterm.tar.gz"
+  sha256 "b8716f4c258f7276b9e4be080db671ace584c748db5403f107c4cee492557d3b"
+  version "0.1.3"
 
   def install
     bin.install "voiceterm"
