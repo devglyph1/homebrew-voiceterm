6446482e34ebf2fa58a27bf0500fff5e760992f8
satishChadive <satish.chandra@cashfree.com>
2025-10-07 16:14:16 +0530

    added support for hindi language

--- Formula/voiceterm.rb
+++ Formula/voiceterm.rb
@@ -1,9 +1,9 @@
 class Voiceterm < Formula
   desc "A voice-powered AI terminal assistant"
   homepage "https://github.com/devglyph1/homebrew-voiceterm"
-  url "https://github.com/devglyph1/homebrew-voiceterm/releases/download/v0.1.5/voiceterm.tar.gz"
+  url "https://github.com/devglyph1/homebrew-voiceterm/releases/download/v0.1.6/voiceterm.tar.gz"
   sha256 "2f52fcb65ae82cfe49cea5aa01c2e6e3f0e84542a5068e4ae07e4332ac50091c"
-  version "0.1.5"
+  version "0.1.6"
 
   def install
     bin.install "voiceterm"
